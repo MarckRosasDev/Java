4ca56ac05861dbaa4e00ac381b318d86f654297e
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-11-08 17:14:56 -0600

    Adición de archico "IndicesCadena.java"

---
+++ Cadenas/src/IndicesCadena.java
@@ -0,0 +1,18 @@
+public class IndicesCadena {
+    public static void main(String[] args) {
+        // Manejo de índices en una cadena
+        var cadena1 = "Hola Mundo";
+
+        // Recuperar el primer caracter
+        var primerCaracter = cadena1.charAt(0); // Recuperar el caracter 'H'
+        System.out.println("primerCaracter = " + primerCaracter);
+
+        // Recuperar el último caracter (en la posición 9)
+        var ultimoCaracter = cadena1.charAt(9);
+        System.out.println("ultimoCaracter = " + ultimoCaracter);
+
+        // Imprimir la letra M, índice 5
+        var letraM = cadena1.charAt(5);
+        System.out.println("letraM = " + letraM);
+    }
+}
